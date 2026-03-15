ab4dd9c3e2098e42d3869ee35ae64774aed7e84a
Jose Antonio <joseantonioalmeida217@gmail.com>
2026-03-15 11:59:09 -0300

    Usando a CBV DetailView com Post para criar PostDetailView

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -134,33 +134,6 @@ class SearchListView(PostListView):
         return super().get(request, *args, **kwargs)
     
 
-def search(request):
-    search_value = request.GET.get('search').strip()
-    posts = (
-        Post.objects.get_published()  #type:ignore
-        .filter(
-            # Título contém search_value OU
-            # Excerpt contém search_value OU
-            # Conteúdo contém search_value 
-            Q(title__icontains=search_value) |
-            Q(excerpt__icontains=search_value) |
-            Q(content__icontains=search_value)
-        )[:PER_PAGE]
-    )
-
-    page_title = f'{search_value[:30]} - '
-
-
-    return render(
-        request,
-        'blog/pages/index.html',
-        {
-            'page_obj': posts,
-            'page_title': page_title,
-            'search_value': search_value,
-        }
-    )
-
 class PageDetailView(DetailView):
     model = Page
     template_name = 'blog/pages/page.html'
@@ -181,19 +154,20 @@ class PageDetailView(DetailView):
             .filter(is_published=True))
 
 
-def post(request, slug):
-    post_obj = Post.objects.get_published().filter(slug=slug).first() #type:ignore
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/pages/post.html'
+    context_object_name = 'post'
+    slug_field = 'slug'
     
-    if post_obj is None:
-        raise Http404()
-
-    page_title = f'{post_obj.title} - '
-
-    return render(
-        request,
-        'blog/pages/post.html', 
-        {
-            'post': post_obj,
-            'page_title': page_title,
-        }
-    )
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        post = self.get_object()
+        page_title = f'{post.title} - Post - ' #type:ignore
+        context.update({
+            'page_title':page_title,
+        }) 
+        return context
+    def get_queryset(self) -> QuerySet[Any]:
+        return super().get_queryset().filter(is_published=True)
+    ...
